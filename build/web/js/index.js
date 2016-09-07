3ce1c4841a9f8887ace5df9857727f26d6221163
58130500113 <58130500113@st.sit.kmutt.ac.th>
2016-09-07 10:05:56 +0700

    Rev. 0

---
+++ build/web/js/index.js
@@ -0,0 +1,27 @@
+function validate() {
+    event.preventDefault();
+    event.stopPropagation();
+    var x = document.forms["chatroom"]["room"].value;
+    var y = document.forms["chatroom"]["username"].value;
+    if (x === null || x === "" || x.checkValidity() === false) {
+        alert("Please enter your room code correctly.");
+        document.getElementById("room").focus();
+    }
+    $('#room').change(function (e) {
+        $('#username').focus();
+    });
+    $('#username').on("focus", function () {
+        if (y === null || y === "") {
+            alert("Please enter your name.");
+            document.getElementById("username").focus();
+        }
+    });
+    $('#username').change(function (e) {
+        $('#join').focus();
+    });
+    if ( !(x === null || x === "") && !(y === null || y === "")) {
+        $(document).ready(function () {
+            $('.modal-trigger').leanModal();
+        });
+    }
+}
