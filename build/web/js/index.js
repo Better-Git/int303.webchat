638952e91f2af7aedb907a0427aa3a04bd21d051
58130500113 <58130500113@st.sit.kmutt.ac.th>
2016-09-19 21:15:44 +0700

    Rev 0.3

--- build/web/js/index.js
+++ build/web/js/index.js
@@ -1,3 +1,64 @@
+document.documentElement.setAttribute('data-platform', navigator.platform);
+document.documentElement.setAttribute('data-useragent', navigator.userAgent);
+
+ko.bindingHandlers.mkPassword = {
+    init: function (element, valueAccessor, allBindingsAccessor, data, context) {
+        var $input = $(element),
+            $enclosingDiv = $('<div>').insertAfter($input),
+            $label = $('<label>'),
+            $span = $('<span>'),
+            id = "password", params = valueAccessor();
+        $input.attr('disabled', true);
+        $input.attr('id', id);
+        $input.attr('minlength', "3");
+        $input.attr('pattern', ".{3,}");
+        $input.attr('title', "Please use any characters at least 3 letters for a password.");
+        $input.attr('type', "password");
+        $label.attr('for', id).text("Password for private room");
+        $span.attr('for', id).text("Password should be at least 3 letters!");
+        $enclosingDiv.attr('id', "input");
+        $enclosingDiv.attr('style', "width: 5cm");
+        $input.addClass('mdl-textfield__input');
+        $label.addClass('mdl-textfield__label');
+        $span.addClass('mdl-textfield__error');
+        $enclosingDiv.addClass("mdl-textfield mdl-js-textfield").append($input).append($label).append($span);
+        ko.bindingHandlers.value.init(element, function () {
+            return params.value;
+        }, allBindingsAccessor, data, context);
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, data, context) {
+        var params = valueAccessor(), value = params.value();
+        ko.bindingHandlers.value.update(element, function () {
+            return params.value;
+        }, allBindingsAccessor, data, context);
+        $(element).parent()[value ? 'addClass' : 'removeClass']('is-dirty');
+        
+    }
+};
+
+var vm = new MyViewModel();
+ko.applyBindings(vm);
+
+function MyViewModel() {
+    var self = this;
+    self.password = ko.observable('');
+    self.resetForm = function () {
+        self.password("");
+    };
+}
+
+function mkPrivate() {
+    $('#mk-private').change(function () {
+        if ($('#mk-private').is(':checked') === true) {
+            $('#password').attr('disabled', false);
+        } else {
+            $('#password').attr('disabled', true);
+            vm.resetForm();
+            $('#password').parent().removeClass('is-invalid');
+        }
+    });
+}
+
 function validate() {
     event.preventDefault();
     event.stopPropagation();
@@ -19,7 +80,7 @@ function validate() {
     $('#username').change(function (e) {
         $('#join').focus();
     });
-    if ( !(x === null || x === "") && !(y === null || y === "")) {
+    if (!(x === null || x === "") && !(y === null || y === "")) {
         $(document).ready(function () {
             $('.modal-trigger').leanModal();
         });
--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,63 @@
+document.documentElement.setAttribute('data-platform', navigator.platform);
 document.documentElement.setAttribute('data-useragent', navigator.userAgent);
-document.documentElement.setAttribute('data-platform', navigator.platform );
+
+ko.bindingHandlers.mkPassword = {
+    init: function (element, valueAccessor, allBindingsAccessor, data, context) {
+        var $input = $(element),
+            $enclosingDiv = $('<div>').insertAfter($input),
+            $label = $('<label>'),
+            $span = $('<span>'),
+            id = "password", params = valueAccessor();
+        $input.attr('disabled', true);
+        $input.attr('id', id);
+        $input.attr('minlength', "3");
+        $input.attr('pattern', ".{3,}");
+        $input.attr('title', "Please use any characters at least 3 letters for a password.");
+        $input.attr('type', "password");
+        $label.attr('for', id).text("Password for private room");
+        $span.attr('for', id).text("Password should be at least 3 letters!");
+        $enclosingDiv.attr('id', "input");
+        $enclosingDiv.attr('style', "width: 5cm");
+        $input.addClass('mdl-textfield__input');
+        $label.addClass('mdl-textfield__label');
+        $span.addClass('mdl-textfield__error');
+        $enclosingDiv.addClass("mdl-textfield mdl-js-textfield").append($input).append($label).append($span);
+        ko.bindingHandlers.value.init(element, function () {
+            return params.value;
+        }, allBindingsAccessor, data, context);
+    },
+    update: function (element, valueAccessor, allBindingsAccessor, data, context) {
+        var params = valueAccessor(), value = params.value();
+        ko.bindingHandlers.value.update(element, function () {
+            return params.value;
+        }, allBindingsAccessor, data, context);
+        $(element).parent()[value ? 'addClass' : 'removeClass']('is-dirty');
+        
+    }
+};
+
+var vm = new MyViewModel();
+ko.applyBindings(vm);
+
+function MyViewModel() {
+    var self = this;
+    self.password = ko.observable('');
+    self.resetForm = function () {
+        self.password("");
+    };
+}
+
+function mkPrivate() {
+    $('#mk-private').change(function () {
+        if ($('#mk-private').is(':checked') === true) {
+            $('#password').attr('disabled', false);
+        } else {
+            $('#password').attr('disabled', true);
+            vm.resetForm();
+            $('#password').parent().removeClass('is-invalid');
+        }
+    });
+}
 
 function validate() {
     event.preventDefault();
@@ -10,20 +68,20 @@ function validate() {
         alert("Please enter your room code correctly.");
         document.getElementById("room").focus();
     }
-    $('#room').change(function(e) {
+    $('#room').change(function (e) {
         $('#username').focus();
     });
-    $('#username').on("focus", function() {
+    $('#username').on("focus", function () {
         if (y === null || y === "") {
             alert("Please enter your name.");
             document.getElementById("username").focus();
         }
     });
-    $('#username').change(function(e) {
+    $('#username').change(function (e) {
         $('#join').focus();
     });
     if (!(x === null || x === "") && !(y === null || y === "")) {
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('.modal-trigger').leanModal();
         });
     }
